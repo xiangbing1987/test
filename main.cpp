a33794638e9383676d43807fb1eb7504881e1ced
xiangbing1987 <xb1987123@163.com>
2019-09-17 14:26:58 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	auto i = 10;
+	cout<<i<<endl;
+	
+	return 0;
+}
